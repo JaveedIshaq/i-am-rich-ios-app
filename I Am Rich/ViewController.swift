26d465b660372bfdd98d789dd39105973a470824
Arslan Khaliq <akhaliq@gtek.solutions>
2019-06-16 21:39:13 +0500

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Javeed Ishaq on 16/06/2019.
+//  Copyright © 2019 Javeed Ishaq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
